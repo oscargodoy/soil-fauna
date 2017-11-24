1de1641e0ebd078ca5f7fc0dd609d10cf64c5e63
Oscar Godoy <ogodoy.re@gmail.com>
2017-11-24 13:28:24 +0100

    new SEM results

--- code/analyses2.R
+++ code/analyses2.R
@@ -211,15 +211,15 @@ dev.off()
 d <- read.csv("data/original_database_r.csv", header=T, sep=",")
 
 # put all units of litter in biomass for the three main species to have same units in (g) as total litter biomass and wood
-env <- d[,c(7:9,11:26)]
-env[,11:15] <- env[,10]* (env[,11:15]/100)
+env <- d[,c(8:10,12:26)]
+env[,11:15] <- env[,10]* (env[,10:14]/100)
 
 #Log the biomass data because differ an order of magnitude. 
-env[,c(10:12, 15:19)] <- log(env[,c(10:12, 15:19)])
-env[7,14] <- log(env[7,14])
+env[,c(9:12, 15:18)] <- log(env[,c(9:12, 15:18)])
+env[7,14] <- log(env[7,14]) # do the log as this is the only value in this column different from zero.
 
 #For analyses eliminate which plot it is, also OLEA because there is not litter in Marrufo forest. 
-env <- env[,c(-1, -13)]
+env <- env[,-12]
 
 #Eliminate wood and non-id litter and others
 env <- env[,c(-12, -13, -17)]
@@ -227,7 +227,7 @@ env <- env[,c(-12, -13, -17)]
 #light plot of samples 21 and 22 have much more light that the others. Therefore que arcsin sqrt transform them to avoid overdispersion in variance
 env[,1]<-asin(sqrt(env[,1]/100))
 
-env <- env[-5, ] # to remove an outlier
+env <- env[-5, ] # to remove an outlier because the humidity level is to high
 
 
 #obtain species (sp) separated from environmental variables (env)
@@ -236,20 +236,20 @@ sp <- d[,27:45]
 sp2<- sp[,c(1:10, 14:16, 18, 19)]
 
 #merge database between decomposers and predators
-decomp <- sp2[, c(1,7:10)]
-predators <-sp2[, c(2:3)]
+decomp <- sp2[, c(1,6:15)]
+predators <-sp2[, c(2:5)]
 
 decomp <- sqrt(decomp)
 decomp <-wisconsin(decomp)
 
-decomp <- decomp[-5, ] # to remove an outlie
+decomp <- decomp[-5, ] # to remove an outlier #DOUBLE CHECK WHETHER THIS IS TRUE. 
 
 
 predators <- sqrt(predators)
 predators <-wisconsin(predators)
 
-predators <- predators[-5, ] # to remove an outlier
-predators <- predators[-1, ] # to remove an outlier
+predators <- predators[-5, ] # to remove an outlier 
+ 
 
 #decomp_sum <- as.vector(apply(decomp,1,FUN=sum))
 #predators_sum <- as.vector(apply(predators,1,FUN=sum))
@@ -287,7 +287,7 @@ res.bv.step.biobio2  <- bv.step(decomp, decomp,
                                 prop.selected.var=0.2, 
                                 num.restarts=50, 
                                 output.best=10, 
-                                var.always.include=c(4)) 
+                                var.always.include=c(3,5,8)) # these are the groups that were included in the frist random run of bv.step analyses. 
 res.bv.step.biobio2 
 
 
@@ -300,23 +300,24 @@ bio.fit
 
 
 #Do the same with the environmental variables, 
-env.fit <- envfit(MDS_res, env[,c(1,5,8)], perm = 999) 
+env.fit <- envfit(MDS_res, env[,c(1,5,14)], perm = 999) 
 env.fit 
 
 #Plot the graphs
 pdf(file="results/NMDS_soil_fauna_decomposers_Fig2.pdf", width=10, height=6) 
 par(mfcol=c(1,2), omi=c(0.1, 0.1, 0.1, 0.1), mar=c(4, 4, 1, 1), ps=8) 
 #first plot related to soil variables
-plot(MDS_res$points, t="n",xlab="NMDS1", ylab="NMDS2", xlim=c(-1, 1), ylim = c(-1,0.5)) 
-plot(env.fit, col="gray10", cex=1.2, font=4) 
+plot(MDS_res$points, t="n",xlab="NMDS1", ylab="NMDS2", xlim=c(-1.1, 1.1), ylim = c(-0.6,0.6)) 
+plot(env.fit, col="gray10", cex=1.2, font=4,  labels = c("Light availability", "Tree defoliation", "High decomposed litter")) 
 text(MDS_res$points, as.character(1:length(MDS_res$points[,1])), cex=1.2, col="red") 
-text(min(MDS_res$points[,1]), max(MDS_res$points[,2]), paste("Stress =",round(MDS_res$stress, 2)), pos=2, font=3, col="gray30") 
+text(min(MDS_res$points[,1]) , max(MDS_res$points[,2]-0.05), paste("Stress =",round(MDS_res$stress, 2)), pos=4, font=3, col="gray30") 
 
 #second plot related to soil fauna groups
-plot(MDS_res$points, t="n",xlab="NMDS1", ylab="NMDS2", xlim=c(-1, 1), ylim = c(-1,0.5)) 
-plot(bio.fit, col="gray10", cex=1.2, font=4) 
+plot(MDS_res$points, t="n",xlab="NMDS1", ylab="NMDS2", xlim=c(-1.1, 1.1), ylim = c(-0.6,0.6)) 
+plot(bio.fit, col="gray10", cex=1.2, font=4, labels = c("Oribatida", "Entomobryomorpha", "Poduromorpha",
+                                                        "Myriapoda", "Enchytraeidae")) 
 text(MDS_res$points, as.character(1:length(MDS_res$points[,1])), cex=1.2, col="red") 
-text(min(MDS_res$points[,1]), max(MDS_res$points[,2]), paste("Stress =",round(MDS_res$stress, 2)), pos=2, font=3, col="gray30") 
+text(min(MDS_res$points[,1]), max(MDS_res$points[,2]-0.05), paste("Stress =",round(MDS_res$stress, 2)), pos=4, font=3, col="gray30") 
 dev.off()
 
 
@@ -329,7 +330,7 @@ sum_decomp <- apply(decomposers, 1, FUN="sum")
 sum_decomp <-sum_decomp[-5]
 
 env2<-cbind(env, sum_decomp)
-env2<- env2[-1, ] # to remove an outlier
+#env2<- env2[-1, ] # to remove an outlier
 ###Do the analyses again grouping insects by groups directly linked to litter. !Eliminate those groups that do not play a direct role. 
 
 source('code/bio.env.R')
@@ -360,7 +361,7 @@ res.bv.step.biobio2  <- bv.step(predators, predators,
                                 prop.selected.var=0.2, 
                                 num.restarts=50, 
                                 output.best=10, 
-                                var.always.include=c(1)) 
+                                var.always.include=c(2)) 
 res.bv.step.biobio2 
 
 
@@ -373,7 +374,7 @@ bio.fit
 
 
 #Do the same with the environmental variables, 
-env.fit <- envfit(MDS_res, env2[,c(1, 6, 11)], perm = 999)  
+env.fit <- envfit(MDS_res, env2[,c(5,7,9,13)], perm = 999)  
 env.fit 
 
 #Plot the graphs
@@ -381,16 +382,16 @@ pdf(file="results/NMDS_soil_fauna_predators_Fig3.pdf", width=10, height=6)
 par(mfcol=c(1,2), omi=c(0.1, 0.1, 0.1, 0.1), mar=c(4, 4, 1, 1), ps=8) 
 #first plot related to soil variables
 plot(MDS_res$points, t="n",xlab="NMDS1", ylab="NMDS2", xlim=c(-1.7, 1.7)) 
-plot(env.fit, col="gray10", cex=1.2, font=4) 
+plot(env.fit, col="gray10", cex=1.2, font=4, labels = c("Tree defoliation", "Litter depth", 
+                                                        "Litter mass", "Medium decomposed litter")) 
 text(MDS_res$points, as.character(1:length(MDS_res$points[,1])), cex=1.2, col="red") 
-text(min(MDS_res$points[,1]), max(MDS_res$points[,2]), paste("Stress =",round(MDS_res$stress, 2)), pos=1, font=3, col="gray30") 
+text(min(MDS_res$points[,1]), max(MDS_res$points[,2]), paste("Stress =",round(MDS_res$stress, 2)), pos=2, font=3, col="gray30") 
 
 #second plot related to soil fauna groups
 plot(MDS_res$points, t="n",xlab="NMDS1", ylab="NMDS2", xlim=c(-1.7, 1.7)) 
-plot(bio.fit, col="gray10", cex=1.2, font=4) 
+plot(bio.fit, col="gray10", cex=1.2, font=4, labels="Prostigmata") 
 text(MDS_res$points, as.character(1:length(MDS_res$points[,1])), cex=1.2, col="red") 
-text(min(MDS_res$points[,1]), max(MDS_res$points[,2]), paste("Stress =",round(MDS_res$stress, 2)), pos=1, font=3, col="gray30") 
-text(x = 0.2, y = 0.50, labels = "mesostigmata", col="gray10", cex=1.2, font=4)
+text(min(MDS_res$points[,1]), max(MDS_res$points[,2]), paste("Stress =",round(MDS_res$stress, 2)), pos=2, font=3, col="gray30") 
 dev.off()
 
 
@@ -400,30 +401,34 @@ dev.off()
 d <- read.csv("data/original_database_r.csv", header=T, sep=",")
 
 # put all units of litter in biomass for the three main species to have same units in (g) as total litter biomass and wood
-env <- d[,c(7:9,11:26)]
-env[,11:15] <- env[,10]* (env[,11:15]/100)
+env <- d[,c(8:10,12:26)]
+env[,11:15] <- env[,10]* (env[,10:14]/100)
 
 #Log the biomass data because differ an order of magnitude. 
-env[,c(10:12, 15:19)] <- log(env[,c(10:12, 15:19)])
-env[7,14] <- log(env[7,14])
+env[,c(9:12, 15:18)] <- log(env[,c(9:12, 15:18)])
+env[7,14] <- log(env[7,14]) # do the log as this is the only value in this column different from zero.
 
 #For analyses eliminate which plot it is, also OLEA because there is not litter in Marrufo forest. 
-env <- env[,c(-1, -13)]
+env <- env[,-12]
 
 #Eliminate wood and non-id litter and others
 env <- env[,c(-12, -13, -17)]
 
 #light plot of samples 21 and 22 have much more light that the others. Therefore que arcsin sqrt transform them to avoid overdispersion in variance
 env[,1]<-asin(sqrt(env[,1]/100))
-##CHECK HOW TO TRANSFORM THE DATA TO AVOID OVERDISPERSION!!!
+
+env <- env[-5, ] # to remove an outlier because the humidity level is to high
 
 
 #obtain species (sp) separated from environmental variables (env)
-sp <- d[,27:42]
+sp <- d[,27:45]
 # focus on those groups that are related to litter decomposition and nutrient cycling processes
-sp2<- sp[,c(1:10, 14:16)]
+sp2<- sp[,c(1:10, 14:16, 18, 19)]
 
-decomp <- sp2[, c(1,6:13)]
+sp2 <- sp2[-5, ]
+
+#merge database between decomposers and predators
+decomp <- sp2[, c(1,6:15)]
 predators <-sp2[, c(2:5)]
 
 
@@ -432,40 +437,92 @@ library(lavaan)
 library(qgraph)
 
 #load env and sp2
-#calculate richness
-richness <- diversity(sp2, index = "shannon", MARGIN = 1, base = exp(1))
-richness_decomp <- diversity(decomp, index = "shannon", MARGIN = 1, base = exp(1))
-richness_predators <- diversity(predators, index = "shannon", MARGIN = 1, base = exp(1))
+#calculate richness it does not longer applies
+#richness <- diversity(sp2, index = "shannon", MARGIN = 1, base = exp(1))
+#richness_decomp <- diversity(decomp, index = "shannon", MARGIN = 1, base = exp(1))
+#richness_predators <- diversity(predators, index = "shannon", MARGIN = 1, base = exp(1))
 
 #calculate abundances
-abundance <- apply(sp2, 1, FUN = "sum")
 abundance_decomp <- apply(decomp, 1, FUN = "sum")
 abundance_predators <- apply(predators, 1, FUN = "sum")
 abundance_oribatida <- decomp[,1]
+abundance_entomobryomorpha <- decomp[,3]
 abundance_poduromorpha <- decomp[,5]
 abundance_miryapoda <- decomp[,8]
 abundance_mesostigmata <-predators[, 1]
+abundance_prostigmata <-predators[, 2]
+abundance_predator_mites <- apply(predators[,1:2], 1, FUN = "sum")
 
-semd <-cbind(env, richness, richness_decomp, richness_predators, abundance, abundance_decomp, 
-             abundance_predators, abundance_oribatida, abundance_poduromorpha, abundance_miryapoda, abundance_mesostigmata
-             )
+
+semd <-cbind(env, abundance_decomp, 
+             abundance_predators, abundance_oribatida, abundance_entomobryomorpha, abundance_poduromorpha, abundance_miryapoda, abundance_mesostigmata,
+             abundance_prostigmata, abundance_predator_mites)
+             
 
 ##model fitting says pathogen variance is too high, log transform for solving 
-semd$pathogen[18]<-0.00001
+semd$pathogen[17]<-1 # this is to do not have -Inf when transforming
 semd$pathogen <- log(semd$pathogen)
 semd$light_plot <- 100*semd$light_plot
 semd$tree_size <- log(semd$tree_size)
-semd$abundance <- log(semd$abundance)
 semd$abundance_decomp <- log(semd$abundance_decomp)
-
-#To avoid problems when logging
-semd$abundance_predators[5] <- 1
 semd$abundance_predators <- log(semd$abundance_predators)
-semd$abundance_oribatida <- log(semd$abundance_oribatida)
+
+# Model that includes the overall effect of the determinants of abundances among decomposers and predators. 
+
+model1 <- '
+# regressions
+
+abundance_decomp ~ tree_size
+abundance_decomp ~ light_plot
+abundance_decomp ~~ pathogen
+
+abundance_predators ~ light_plot
+abundance_predators ~ tree_size
+abundance_predators ~~ pathogen
+
+# correlation
+abundance_predators ~~ abundance_decomp
+light_plot ~~ tree_size
+pathogen ~~ tree_size
+
+
+#latent variables
+
+#intercept
+
+'
+
+fit <- sem(model1, data=semd, fixed.x=FALSE)
+summary(fit, standardized=TRUE)
+#this function is to obtain the fit form the model see http://lavaan.ugent.be/tutorial/tutorial.pdf
+fitMeasures(fit, c("cfi","rmsea","srmr", "pvalue"))
+
+corrgram(semd[,1:16], order=TRUE, lower.panel=panel.conf, upper.panel=panel.pts, text.panel=panel.txt,
+         diag.panel=panel.density, pch=16, lty=1, main="Environmental correlations")
+
+##represent the results. 
+library(qgraph)
+library(htmlwidgets)
+library(semPlot)
+
+#Standarized plot diagram http://sachaepskamp.com/semPlot/examples for examples
+semPaths(fit, what = "std", whatLabels = 'std', style= "ram", layout= "spring", residuals= FALSE, 
+         sizeInt =500, edge.label.cex = 1.5, node.width=2, node.height=2, label.cex= 3, curvePivot = TRUE)
+
+
+
+
+
+
+
 
 #To avoid problems when logging
+semd$abundance_oribatida <- log(semd$abundance_oribatida)
 semd$abundance_mesostigmata[5] <- 1
-semd$abundance_mesostigmata <- log(semd$abundance_mesostigmata)
+abundance_entomobryomorpha <- decomp[,3]
+abundance_poduromorpha <- decomp[,5]
+abundance_miryapoda <- decomp[,8]
+abundance_prostigmata <-predators[, 2]
 
 #model 1 describes the best SEM model for decomposers
 model1 <- '
@@ -547,37 +604,7 @@ pathogen~~ tree_size
 fit <- sem(model5, data=semd, fixed.x=FALSE)
 summary(fit, standardized=TRUE)
 
-# Model that includes the overall effect of the determinants of abundances among decomposers and predators. 
-
-model6 <- '
-# regressions
-
-abundance_decomp ~ litter_depth
-abundance_decomp ~ light_plot
-abundance_decomp ~ pathogen
-
-litter_depth ~ tree_size
-
-# correlation
-abundance_predators ~~ abundance_decomp
-'
-
-fit <- sem(model6, data=semd, fixed.x=FALSE)
-summary(fit, standardized=TRUE)
-#this function is to obtain the fit form the model see http://lavaan.ugent.be/tutorial/tutorial.pdf
-fitMeasures(fit)
-
-##represent the results. 
-library(qgraph)
-library(htmlwidgets)
-library(semPlot)
-
-#Standarized plot diagram http://sachaepskamp.com/semPlot/examples for examples
-semPaths(fit, what = "std", whatLabels = 'std', style= "ram", layout= "spring", residuals= FALSE, 
-sizeInt =500, edge.label.cex = 1.5, node.width=2, node.height=2, label.cex= 3, curvePivot = TRUE)
-
-
-
+####HASTA AQUI!!!
 
 ##These are glm analyses to check better the broad scale varaition in soil fauna abundance
 
